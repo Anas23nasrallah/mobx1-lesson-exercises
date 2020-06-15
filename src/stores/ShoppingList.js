b3df4fcbdad22ca7642c371ed6741e8894905d94
AnasNasrallah <anas.23.nasrallah@gmail.com>
2020-06-15 14:55:00 +0300

    exercises done

--- src/stores/ShoppingList.js
+++ src/stores/ShoppingList.js
@@ -1,22 +1,31 @@
 import { observable, action } from 'mobx'
 import { Item } from './Item'
+import { name } from 'file-loader';
 
 
 export class ShoppingList {
     // your code here
-    list = [];
-    length;
+    @observable list = [];
+    @observable length;
+
     checkItem = () => {
         // your code here
     }
-    addItem = () => {
-        // your code here
+    @action addItem = (name) => {
+        this.list.unshift(new Item(name))
     }
-    editItem = () => {
-        // your code here
-    }
-    deleteItem = () => {
-        // your code here
+
+    @action deleteItem = (nameToDelete) => {
+        for(let i in this.list){
+            if(this.list[i].name === nameToDelete){
+                this.list.splice(i, 1)
+                break
+            }
+        }
     } 
+    
+    @action editItem = (itemName, newLocation) => {
+        this.list.find(i => i.name === itemName).location = newLocation
+    }
 }
 
